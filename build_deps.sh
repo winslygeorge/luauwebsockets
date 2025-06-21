0a1f3875966826a91128e3f95abcd4d6f5b8621f
winslygeorge <omondiwinsly2@gmail.com>
2025-06-21 20:05:49 +0300

    install depedencies for uWebsockets

--- build_deps.sh
+++ build_deps.sh
@@ -5,6 +5,12 @@ set -e
 # Define destination directory
 UWS_DIR="uWebSockets"
 
+# === 0. Install Build Dependencies ===
+echo "[0/6] Installing system dependencies..."
+sudo apt update
+sudo apt install -y git cmake python3 g++ libssl-dev libpq-dev zlib1g-dev lua5.1 luajit libluajit-5.1-dev
+
+
 # Clone uWebSockets with submodules if not already present
 if [ ! -d "$UWS_DIR" ]; then
     echo "Cloning uWebSockets with submodules..."
